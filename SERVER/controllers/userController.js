cec767c69d9fcd1d058c44882c14174aeec5dd8d
Adarsh Dubey <adarshdubeydelhi@gmail.com>
2025-08-23 09:59:16 +0530

    changes in server

--- SERVER/controllers/userController.js
+++ SERVER/controllers/userController.js
@@ -1,4 +1,4 @@
-const {User} = require("../models/userModel.js");
+const { User } = require("../models/userModel.js");
 const bcrypt = require("bcrypt");
 const { createSecretToken } = require("../Util/ScecretToken.js")
 
@@ -22,7 +22,7 @@ const Signup = async (req, res, next) => {
 
         //CREATE THE NEWUSER FOR ADDING IT TO THE DATABASE 
         const newUser = new User({
-            name : name,
+            name: name,
             email: email,
             password: hashedpassword,
             created_At: created_At
@@ -36,13 +36,15 @@ const Signup = async (req, res, next) => {
         res.cookie("token", token, {
             withCredentials: true,
             httpOnly: false,
+            secure: true,           // required for HTTPS (Render is HTTPS)
+            sameSite: "none",
         })
 
-       return res.status(201).json({ message: "USER SIGNED IN SUCCESSFULLY ", success: true, user });
+        return res.status(201).json({ message: "USER SIGNED IN SUCCESSFULLY ", success: true, user });
 
     } catch (error) {
 
-        console.log("Something went wrong : "+error)
+        console.log("Something went wrong : " + error)
 
     }
 }
@@ -52,56 +54,58 @@ const Login = async (req, res, next) => {
     try {
 
         //WE FETCHED THE EMAIL AND PASSWORD THAT GIVEN BY THE USER 
-        const {email, password}= req.body;
+        const { email, password } = req.body;
 
         //CHECK IS EMAIL AND PASSWORD ARE ENTERED BY THE USER OR NOT 
-        if(!email || !password){
-    
-           return res.json({"message":"ALL FIELDS ARE REQUIRED "});
+        if (!email || !password) {
+
+            return res.json({ "message": "ALL FIELDS ARE REQUIRED " });
         }
-    
+
         // FIND OUT THAT THE USER EXISTS OR NOT IN OUR DATABASE ACCORDING TO THE GIVEN EMAIL 
-        const user = await User.findOne({email});
-    
+        const user = await User.findOne({ email });
+
         //CHECK IS OUR USER EXISTS OR NOT 
-        if(!user){
-           return res.json({"message":"INCORRECT EMAIL AND PASSWORD "})
+        if (!user) {
+            return res.json({ "message": "INCORRECT EMAIL AND PASSWORD " })
         }
-    
+
         // IF THE USER EXISTS THEN WE COMPARE THE GIVEN PASSWORD TO THE EXISTING PASSWORD IN THE DATABASE 
-        const auth = await bcrypt.compare(password , user.password);
-    
+        const auth = await bcrypt.compare(password, user.password);
+
         //CHECK IS THAT IS PASSWORD ARE MATCHED OR NOT 
-        if(!auth ){
-            return res.json({"message":"INCORRECT EMAIL AND PASSWORD "})
+        if (!auth) {
+            return res.json({ "message": "INCORRECT EMAIL AND PASSWORD " })
         }
-    
+
 
         // IF THE PASSWORD IS MATCHED THEN WE CREATE A TOKEN BY USING THE CREATESCRETETOKEN 
         const token = createSecretToken(user._id);
-    
+
         res.cookie("token", token, {
             withCredentials: true,
             httpOnly: false,
-          });
-    
-          res.status(201).json({ message: "User logged in successfully", success: true });
-        
+            secure: true,           // required for HTTPS (Render is HTTPS)
+            sameSite: "none",
+        });
+
+        res.status(201).json({ message: "User logged in successfully", success: true });
+
     } catch (error) {
 
-        console.log("SOMETHING WENT WRONG : "+error);
-        res.status(500).json({ message: "Internal Server Error"});
-        
+        console.log("SOMETHING WENT WRONG : " + error);
+        res.status(500).json({ message: "Internal Server Error" });
+
     }
 
-   
+
 
 }
 
-module.exports={Signup,Login};
+module.exports = { Signup, Login };
 
 
 
-   
+
 
 
